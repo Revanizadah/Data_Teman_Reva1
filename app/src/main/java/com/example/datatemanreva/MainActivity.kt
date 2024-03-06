f4be9494a8e9b56fb2360c9f32b4521faf80da1c
Revaniza <revani_22310008@stimata.ac.id>
2024-03-06 14:30:30 +0700

    Initial commit

--- app/src/main/java/com/example/datatemanreva/MainActivity.kt
+++ app/src/main/java/com/example/datatemanreva/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.datatemanreva
 
+import android.content.Intent
+import android.media.MediaPlayer.OnCompletionListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.datatemanreva.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var auth:FirebaseAuth? = null
+    private val RC_SIGN_IN = 1
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener(this)
+        binding.save.setOnClickListener(this)
+        binding.showData.setOnClickListener(this)
+
+        auth = FirebaseAuth.getInstance()
     }
+
+    private fun isEmpty(s: String): Boolean {
+        return TextUtils.isEmpty(s)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.save -> {}
+            R.id.logout -> {
+                AuthUI.getInstance().signOut(this@MainActivity).addOnCompleteListener(object :
+                    OnCompleteListener<Void> {
+                    override fun onComplete(p0: Task<Void>) {
+                        Toast.makeText(this@MainActivity, "Logout Berhasil", Toast.LENGTH_SHORT).show()
+                        intent = Intent(this@MainActivity, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP // Menambahkan flag untuk membersihkan tumpukan aktivitas
+                        startActivity(intent)
+                        finish()
+                    }
+                })
+            }
+            R.id.show_data -> {}
+        }
+    }
+
 }
